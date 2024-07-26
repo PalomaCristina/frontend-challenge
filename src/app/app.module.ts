f7d720a7ba48cafa45becda78927cb9e38311b8f
palomacorrea <paloma.correa@dnkinfotelecom.com.br>
2024-07-26 18:21:35 -0300

    style the header bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    CardComponent
+    CardComponent,
   ],
   imports: [
     BrowserModule,
