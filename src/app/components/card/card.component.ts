001993b87827b713a878cdaa97941f7bb6964944
PalomaCristina <paloma1899@gmail.com>
2024-07-27 16:41:06 -0300

    feat: include the app container and app card

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent {
 
+export class CardComponent {
+  @Input() src: string = ''
+  @Input() alt: string = ''
 }
