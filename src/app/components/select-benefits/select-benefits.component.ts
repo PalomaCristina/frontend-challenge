e95d92e4401424cb2ec1eed653af78b0a079b335
PalomaCristina <paloma1899@gmail.com>
2024-07-28 17:47:29 -0300

    fix: benefits icons and position + and h1 of home component

--- src/app/components/select-benefits/select-benefits.component.ts
+++ src/app/components/select-benefits/select-benefits.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-select-benefits',
   templateUrl: './select-benefits.component.html',
@@ -8,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 export class SelectBenefitsComponent implements OnInit {
   benefits = [
     { icon: '../../../assets/images/icon-block.png', text: 'cartões sem anuidade' },
-    { icon: '../../../assets/images/icon-buying', text: 'cartões de supermercados' },
-    { icon: '../../../assets/images/icon-airplane', text: 'cartões de milhas e viagens' },
-    { icon: '../../../assets/images/icon-buying.png', text: 'cartões de games' }
+    { icon: '../../../assets/images/icon-buying.png', text: 'cartões de supermercados' },
+    { icon: '../../../assets/images/icon-airplane.png', text: 'cartões de milhas e viagens' },
+    { icon: '../../../assets/images/icon-game.png', text: 'cartões de games' }
   ];
 
   constructor() {}
